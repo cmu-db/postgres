b6d713ac093837d88c31ed4b8d06d30f7d7cfd02
Wan Shen Lim <wanshen.lim@gmail.com>
2021-08-29 15:47:04 -0400

    IT LIVES!

--- cmudb/env/docker-entrypoint.sh
+++ cmudb/env/docker-entrypoint.sh
@@ -15,10 +15,16 @@ POSTGRES_INITDB_WALDIR=${POSTGRES_INITDB_WALDIR}
 POSTGRES_HOST_AUTH_METHOD=${POSTGRES_HOST_AUTH_METHOD}
 PGDATA=${PGDATA}
 
-# From our own Docker image.
+# The section below lists custom variables for our project.
 
+# General purpose.
 BIN_DIR=${BIN_DIR}  # Folder containing all the PostgreSQL binaries.
 PGPORT=${PGPORT}    # The port to listen on.
+
+# Replication.
+NP_REPLICATION_TYPE=${NP_REPLICATION_TYPE}          # Must be "primary" or "replica"
+NP_REPLICATION_USER=${NP_REPLICATION_USER}          # Replication user.
+NP_REPLICATION_PASSWORD=${NP_REPLICATION_PASSWORD}  # Replication password.
 
 # =====================================================================
 # Default environment variable values.
@@ -68,8 +74,12 @@ _pg_config() {
   AUTO_CONF=${PGDATA}/postgresql.auto.conf
   HBA_CONF=${PGDATA}/pg_hba.conf
 
-  echo "listen_addresses = '*'" >> ${AUTO_CONF}
+  # pg_hba.conf
   echo "host all all 0.0.0.0/0 ${POSTGRES_HOST_AUTH_METHOD}" >> ${HBA_CONF}
+
+  # postgresql.auto.conf
+  # Allow Docker host to connect to container.
+  echo "listen_addresses = '*'" >> ${AUTO_CONF}
 }
 
 _pg_create_user_and_db() {
@@ -78,9 +88,32 @@ _pg_create_user_and_db() {
 }
 
 _pg_setup_replication() {
-  ${BIN_DIR}/psql -c "create user ${NP_REPLICATION_USER} with replication encrypted password '${NP_REPLICATION_PASSWORD}'" postgres
-  ${BIN_DIR}/psql -c "select pg_create_physical_replication_slot('replication_slot_replica1')" postgres
+  AUTO_CONF=${PGDATA}/postgresql.auto.conf
+  HBA_CONF=${PGDATA}/pg_hba.conf
 
+  # See PostgreSQL docs for complete description of parameters.
+
+  # wal_level: How much information to ship over.
+  echo "wal_level = replica" >> ${AUTO_CONF}
+  # hot_standby: True to enable connecting and running queries during recovery.
+  echo "hot_standby = on" >> ${AUTO_CONF}
+  # max_wal_senders: Maximum number of concurrent connections to standby/backup clients.
+  echo "max_wal_senders = 10" >> ${AUTO_CONF}
+  # max_replication_slots: Maximum number of replication slots.
+  echo "max_replication_slots = 10" >> ${AUTO_CONF}
+  # hot_standby_feedback: True if standby should tell primary about what queries are currently executing.
+  echo "hot_standby_feedback = on" >> ${AUTO_CONF}
+
+  if [ "${NP_REPLICATION_TYPE}" = "primary" ]; then
+    # Create replication user.
+    ${BIN_DIR}/psql -c "create user ${NP_REPLICATION_USER} with replication encrypted password '${NP_REPLICATION_PASSWORD}'" postgres
+    # Allow replication user to connect..
+    echo "host replication ${NP_REPLICATION_USER} 0.0.0.0/0 md5" >> ${HBA_CONF}
+    # Reload configuration.
+    ${BIN_DIR}/psql -c "select pg_reload_conf()" postgres
+    # Create replication slot for replica.
+    ${BIN_DIR}/psql -c "select pg_create_physical_replication_slot('replication_slot_replica1')" postgres
+  fi
 }
 
 # All the steps required to start up PostgreSQL.
@@ -89,6 +122,10 @@ _pg_start_all() {
   _pg_config              # Write any configuration options required.
   _pgctl_start            # Start the PostgreSQL cluster.
   _pg_create_user_and_db  # Create the specified user and database.
+
+  if [ ! -z "${NP_REPLICATION_TYPE}" ]; then
+    _pg_setup_replication
+  fi
 }
 
 # =====================================================================
@@ -96,9 +133,28 @@ _pg_start_all() {
 # =====================================================================
 
 main() {
-  _pg_start_all
-  _pg_stop
-  _pg_start
+  # Only initdb if this is not a replica. The replica will recover from backup.
+  if [ ! "${NP_REPLICATION_TYPE}" = "replica" ]; then
+    # This is a single-node or the primary.
+    _pg_start_all
+    _pg_stop
+    _pg_start
+  else
+    # This is a replica.
+    while true ; do
+      # Note that there is a potential race here where the primary restarts and healthcheck succeeds.
+      sleep 10
+      ${BIN_DIR}/pg_isready --host primary --port 15721 --username noisepage
+      READY_CHECK=$?
+      if [ "$READY_CHECK" = "0" ]; then
+        break
+      fi
+    done
+
+    # Initialize replica backup from primary.
+    echo passyMcPassword | ${BIN_DIR}/pg_basebackup --host primary --username replicator --port 15721 --pgdata=${PGDATA} --format=p --wal-method=stream --progress --write-recovery-conf
+    _pg_start
+  fi
 }
 
 main
